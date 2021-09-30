7ee534845b00b471712cfeaea081ae26065e2c9f
Profesor-02 <72236273+Profesor-02@users.noreply.github.com>
2021-09-30 18:41:27 -0300

    errores

--- js/java.js
+++ js/java.js
@@ -1,11 +1,10 @@
-// HAMBURGUESA
-// CARNE 
+/*
+//---------test enviar opciones----------//
 var mixcarne = document.getElementById("op0.1");
 var carne = document.getElementById("op0.2");
 var pollo = document.getElementById("op0.3");
 var vegana = document.getElementById("op0.4");
 var error = document.getElementById("error");
-
 
 function enviarCarne(){
     console.log("Enviando formulario...")
@@ -17,13 +16,34 @@ function enviarCarne(){
     
     return false;
 }
+*/
 
-function myFunction1() {
+//------------checkeo pedido---------------------//
+function pedidoseguro() {
     var txt;
     if (confirm("Si esta segur@ de enviar el pedido por favor continue.")) {
-      txt = "You pressed OK!";
+      txt = "SI"; 
     } else {
-      txt = "You pressed Cancel!";
+      txt = "NO";
     }
-    document.getElementById("demo").innerHTML = txt;
+    document.getElementById("demo1").innerHTML = txt;
   }
+//--------------alertas formulario------------------//
+function formulario(){
+  formularioN("comentnombre","nombre")
+  formularioN("comentapellido","apellido")
+}
+
+function formularioN(parametro1, parametro2) {
+  const message = document.getElementById(parametro1)
+  message.InnerHTML = " ";
+  let x = document.getElementById(parametro2).value;
+  try { 
+    if(x == "")  throw " introduzca " + parametro2;
+    else throw "bien";
+  }
+  catch(err) {
+    if(err == "bien") message.innerHTML = " ";
+    else message.innerHTML = "*Por favor, " + err;
+  }
+}
