7e3f41a1eed7e58bcacd0d2934fc5f1422cb3654
Profesor-02 <72236273+Profesor-02@users.noreply.github.com>
2021-10-12 20:36:33 -0300

    lo en la semana
    cambie los Ids de la comida e hice un excel con las combinaciones. Lo termino más tarde. Deje el js con el formato del anterior, solo queda reemplazar

--- js/java.js
+++ js/java.js
@@ -1,22 +1,3 @@
-/*
-//---------test enviar opciones----------//
-var mixcarne = document.getElementById("op0.1");
-var carne = document.getElementById("op0.2");
-var pollo = document.getElementById("op0.3");
-var vegana = document.getElementById("op0.4");
-var error = document.getElementById("error");
-
-function enviarCarne(){
-    console.log("Enviando formulario...")
-    
-    var mensajesError = [];
-    if((mixcarne.value === null || mixcarne.value === '' ) && (carne.value === null || carne.value === '') &&( pollo.value === null || pollo.value === '' ) && (vegana.value === null || vegana.value === '' )){
-        mensajesError.push('Selecciona una Carne');
-    }
-    
-    return false;
-}
-*/
 
 //------------checkeo pedido---------------------//
 function pedidoseguro() {
@@ -35,6 +16,23 @@ function formulario(){
 }
 
 function formularioN(parametro1, parametro2) {
+  const message = document.getElementById(parametro1)
+  message.InnerHTML = " ";
+  let x = document.getElementById(parametro2).value;
+  try { 
+    if(x == "")  throw " introduzca " + parametro2;
+    else throw "bien";
+  }
+  catch(err) {
+    if(err == "bien") message.innerHTML = " ";
+    else message.innerHTML = "*Por favor, " + err;
+  }
+}
+//--------------el tedioso------------------//
+function quiero_ordenar(){
+  funcion_larga("La_orden","La_orden") // reemplazar el parametro2 por el id de la comida
+}
+function funcion_larga(La_orden,parametro2) { // reemplazar el parametro2 por el id de la comida
   const message = document.getElementById(parametro1)
   message.InnerHTML = " ";
   let x = document.getElementById(parametro2).value;
